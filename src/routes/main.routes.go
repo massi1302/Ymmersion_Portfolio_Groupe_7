c18ab9754f6654353aaa661a138bd8e5ebe65042
massi1302 <ahfirmassi2@gmail.com>
2025-02-04 09:29:49 +0100

    page des projets1

--- src/routes/main.routes.go
+++ src/routes/main.routes.go
@@ -8,6 +8,7 @@ import (
 func InitServe() {
 
 	homeRoutes()
+	projetsRoutes()
 	fmt.Println("Le serveur est opérationel : http://localhost:8080")
 	http.ListenAndServe("localhost:8080", nil)
 }
--- src/routes/projet.route.go
+++ src/routes/projet.route.go
@@ -5,6 +5,6 @@ import (
 	"net/http"
 )
 
-func homeRoutes() {
-	http.HandleFunc("/", controllers.HomePage)
+func projetsRoutes() {
+	http.HandleFunc("/projets", controllers.ProjetsPage)
 }
