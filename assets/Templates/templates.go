428ee17187332999e81dc8db5d8fbfe5cfe0d269
7n4xt <abdul.esughi@gmail.com>
2025-02-04 08:54:42 +0100

    fixed the error

--- assets/Templates/templates.go
+++ assets/Templates/templates.go
@@ -14,7 +14,7 @@ var Temp *template.Template
 func InitTemplates() {
 	fileServer := http.FileServer(http.Dir("./assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fileServer))
-	temp, tempErr := template.ParseGlob("./Templates/*.html")
+	temp, tempErr := template.ParseGlob("./assets/Templates/*.html")
 	if tempErr != nil {
 		fmt.Printf("Erreur Template - Une erreur lors du chargement des template \n message d'erreur : %v\n", tempErr.Error())
 		os.Exit(1)
