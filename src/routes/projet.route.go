71e2a52268d674f15a42536d9a20cecf29923c1b
massi1302 <ahfirmassi2@gmail.com>
2025-02-05 09:59:39 +0100

    fix2

--- src/routes/projet.route.go
+++ src/routes/projet.route.go
@@ -5,19 +5,7 @@ import (
 	"net/http"
 )
 
-// ProjetsRoutes doit être exportée (majuscule)
 func ProjetsRoutes() {
 	http.HandleFunc("/index", controllers.HomePage)
 	http.HandleFunc("/projets", controllers.ProjetsPage)
-	http.HandleFunc("/guess-number", controllers.GuessNumberGameHandler)
-}
-
-// Handler séparé pour le jeu
-func GuessNumberHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet, http.MethodPost:
-		controllers.GuessNumberGameHandler(w, r)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
 }
